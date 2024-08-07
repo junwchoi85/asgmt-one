afe81e9e439e7925726ff2c7f65345bf0d5ae5a8
Jun Hwan <junwchoi85@gmail.com>
2024-08-08 10:27:34 +1200

    Initial commit. local database setup using sqlite3

---
+++ src/setup/database_setup.py
@@ -0,0 +1,74 @@
+import sqlite3
+
+def setup_database():
+    conn = sqlite3.connect('mseboo.db')
+    c = conn.cursor()
+
+    # Drop tables if they exist
+    c.execute('DROP TABLE IF EXISTS user;')
+    c.execute('DROP TABLE IF EXISTS role;')
+    c.execute('DROP TABLE IF EXISTS user_role;')
+    c.execute('DROP TABLE IF EXISTS car;')
+    c.execute('DROP TABLE IF EXISTS booking;')
+
+    # Create tables
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS user (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_code VARCHAR(6) NOT NULL,
+            username VARCHAR(45) NOT NULL,
+            password VARCHAR(45) NOT NULL,
+            UNIQUE (user_code)
+        );
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS role (
+            role_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            role_code VARCHAR(6) NOT NULL,
+            desc VARCHAR(45)
+        );
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS user_role (
+            user_id INTEGER NOT NULL,
+            role_id INTEGER NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+            FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE RESTRICT ON UPDATE RESTRICT
+        );
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS car (
+            car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            car_code VARCHAR(6) NOT NULL,
+            make VARCHAR(45),
+            model VARCHAR(45),
+            year INTEGER,
+            mileage VARCHAR(45),
+            availability INTEGER,
+            UNIQUE (car_code)
+        );
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS booking (
+            bkg_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            bkg_code VARCHAR(6),
+            user_id INTEGER,
+            car_id INTEGER,
+            start_date VARCHAR(45),
+            end_date VARCHAR(45),
+            total_fee VARCHAR(45),
+            status VARCHAR(3),
+            FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+            FOREIGN KEY (car_id) REFERENCES car (car_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+        );
+    ''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    setup_database()
