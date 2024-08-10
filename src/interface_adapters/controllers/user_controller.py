485128b79b0f19f32d919bf10e0f80756a590e47
Jun Hwan <junwchoi85@gmail.com>
2024-08-10 21:27:58 +1200

    refactoring

--- src/interface_adapters/controllers/user_controller.py
+++ src/interface_adapters/controllers/user_controller.py
@@ -1,10 +1,10 @@
-from use_cases.user_use_case import CreateUser
+from use_cases.user_use_case import UserUseCase
 
 class UserController:
     # Initialize the user controller with the user repository
     def __init__(self, user_repo):
-        self.create_user_use_case = CreateUser(user_repo)
+        self.create_user_use_case = UserUseCase(user_repo)
 
     # Create a user
     def create_user(self, user):
-        return CreateUser(self.user_repo).execute(user)
+        return UserUseCase(self.user_repo).createUser(user)
