93daf03bce99285e45159ad6b890481e2a46cd63
Jun Hwan <junwchoi85@gmail.com>
2024-08-23 05:10:45 +1200

    constants managment

---
+++ src/constants/cli_constants.py
@@ -0,0 +1,20 @@
+# Response status codes
+STATUS_SUCCESS = "success"
+STATUS_FAILURE = "failure"
+
+# Response messages
+MESSAGE_SIGN_UP_SUCCESS = "Sign up successful"
+MESSAGE_SIGN_UP_FAILURE = "Sign up failed. Please contact support"
+MESSAGE_USERNAME_PASSWORD_REQUIRED = "Username and password are required"
+MESSAGE_SIGN_IN_SUCCESS = "Sign in successful"
+MESSAGE_SIGN_IN_FAILURE = "Sign in failed"
+
+MESSAGE_START_END_DATE_REQUIRED = "Start date and end date are required"
+MESSAGE_INVALID_DATE_FORMAT = "Invalid date format. Date must be in the format YYYY-MM-DD"
+MESSAGE_START_DATE_BEFORE_END_DATE = "Start date must be before end date"
+MESSAGE_START_DATE_IN_FUTURE = "Start date must be in the future"
+
+MESSAGE_BOOKING_SUCCESS = "Booking successful"
+MESSAGE_BOOKING_FAILURE = "Booking failed. Please contact support"
+
+MESSAGE_CONTACT_SUPPORT = "Please contact support"
